177730e53d53ffe7f5574adb2437596ea525c915
naming <namingbe@users.noreply.github.com>
2025-08-06 04:00:36 +0200

    add vec-add solutions

--- vector-addition/solution-tinygrad.py
+++ vector-addition/solution-tinygrad.py
@@ -3,4 +3,4 @@ import tinygrad
 
 # A, B, C are tensors on the GPU
 def solve(A: tinygrad.Tensor, B: tinygrad.Tensor, C: tinygrad.Tensor, N: int):
-    pass
+    C.copy_(A + B)
--- vector-addition/solution.cu
+++ vector-addition/solution.cu
@@ -2,7 +2,8 @@
 #include <cuda_runtime.h>
 
 __global__ void vector_add(const float* A, const float* B, float* C, int N) {
-
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < N) C[i] = A[i] + B[i];
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
